379ca6957061b2eea7dd3b0cdc25d791eda83080
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2021-12-28 16:12:46 -0800

    Peanut butter jelly time

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Have you ever had a DUI");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int noticket = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = age > 15 && dui == false && noticket < 3;
+            Console.ReadLine();
+            Console.Write(qualified);
+        }
+    }
+}
