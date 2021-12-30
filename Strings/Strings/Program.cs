8570d4e7374f37fd188a9847887ea111b351e1f8
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2021-12-30 15:23:40 -0800

    Please forgive my terrible rap string builder

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("String practice");
+            Console.ReadLine();
+            string a = "one";
+            string b = "two";
+            string c = "three";
+            string concat = a + b + c;
+            Console.WriteLine(a + b + c);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Bryce,");
+            sb.Append(" and I don't like rice,");
+            sb.Append(" my mom says I'm nice,");
+            sb.Append(" like george bush can't get fooled twice.");
+
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+            concat = concat.ToUpper();
+            Console.WriteLine(concat);
+            Console.ReadLine();
+        }
+    }
+}
