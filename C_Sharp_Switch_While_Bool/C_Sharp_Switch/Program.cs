7cfbef625491de2f2f8ddc2146f89850e482f2a3
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2021-12-30 14:24:34 -0800

    while bool loop

---
+++ C_Sharp_Switch_While_Bool/C_Sharp_Switch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace C_Sharp_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.WriteLine("Guess a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+            while (!isGuessed)
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62 try again");
+                        Console.WriteLine("Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29 try again");
+                        Console.WriteLine("Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55 try again");
+                        Console.WriteLine("Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12. Nice!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong");
+                        Console.WriteLine("Guess again");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            
+            Console.Read();
+        }
+    }
+}
