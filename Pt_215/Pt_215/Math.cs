7e5b54f9bf79ec07229f59b4abc32f5ed5b2f01e
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-06 14:34:13 -0800

    Pt_215

---
+++ Pt_215/Pt_215/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pt_215
+{
+    class Math
+    {
+        public int Math1(int dsad)
+        {          
+            int result1 = dsad * 5;
+            return result1;          
+        }
+
+        public int Blah(int fff)
+        {        
+            int result2 = fff * 10;
+            return result2;
+        }
+
+        public int BlahBlah(int ffff)
+        {
+            int result3 = ffff * 20;
+            return result3;
+        }
+
+
+
+    }
+}
---
+++ Pt_215/Pt_215/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pt_215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Math = new Math();
+            //calling method one to multiply by 5
+            Console.WriteLine("What number would you like to do math operations on?");
+            int One = Convert.ToInt32(Console.ReadLine());
+            int whatever = Math.Math1(One);
+            Console.WriteLine(whatever);
+            Console.ReadLine();
+
+            //calling method one to multiply by 10
+            Console.WriteLine("What number would you like to do math operations on?");
+            int Two = Convert.ToInt32(Console.ReadLine());
+            int whatever2 = Math.Blah(Two);
+            Console.WriteLine(whatever2);
+            Console.ReadLine();
+
+            //calling method one to multiply by 20
+            Console.WriteLine("What number would you like to do math operations on?");
+            int Three = Convert.ToInt32(Console.ReadLine());
+            int whatever3 = Math.BlahBlah(Three);
+            Console.WriteLine(whatever3);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
