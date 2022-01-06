73c08553210b2c603267e6aae35da813bb014a29
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-06 15:53:29 -0800

    pt_219

---
+++ pt_219/pt_219/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace pt_219
+{
+    class Math
+    {
+        public int MathA(int A, [Optional]int B)
+        {
+            int result = A + B;
+            return result;
+        }
+    }
+}
---
+++ pt_219/pt_219/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace pt_219
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter two numbers one at a time, the second number is optional");
+            Console.WriteLine("First number please:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("And the second (if you want)");
+                int B = Convert.ToInt32(Console.ReadLine());
+                int result = math.MathA(A, B);
+                Console.WriteLine(result);
+                
+            }
+            catch
+            {
+                int result = math.MathA(A);
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
