59511e2deba0c46d1dfce80dbbe91360278f0f4c
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-07 14:48:13 -0800

    Add files via upload

---
+++ pt_220/pt_220/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pt_220
+{
+    class Math
+    {
+        public void MathA(int A, int B)
+        {
+            int result = A + 5;
+            Console.WriteLine(result);
+            Console.WriteLine(B);
+        }
+    }
+}
---
+++ pt_220/pt_220/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pt_220
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+
+            Console.WriteLine("Please enter two parameters");
+
+            Console.WriteLine("First parameter");
+            int A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second parameter");
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            math.MathA(A, B);
+            math.MathA(A: 3, B: 5);
+            Console.ReadLine();
+        }
+    }
+}
