73cbcb7401482ba9b8c974a1ef5f7e09659713bf
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-07 14:04:09 -0800

    pt_218_fixed

---
+++ Pt_218/Pt_218/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pt_218
+{
+    class Math
+    {
+        public int MathA(int A)
+        {
+            int result = A + 5;
+            return result;
+        }
+
+        public int MathA(decimal A)
+        {
+            int X = Convert.ToInt32(A);
+            int result1 = X * 5;
+            return result1;
+        }
+
+        public int MathA(string A)
+        {        
+            int result2 = Convert.ToInt32(A) + 7;          
+            return result2;
+        }
+
+    }
+}
