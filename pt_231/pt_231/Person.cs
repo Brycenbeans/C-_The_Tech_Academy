af383d823bf9da07fc90832f20470382ee017e9f
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-08 15:16:00 -0800

    pt_231

---
+++ pt_231/pt_231/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pt_231
+{
+    public class Person 
+    {
+        public string FirstName;
+        public string LastName;
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+
+    }
+    public class Employee : Person
+    {
+        public int Id;
+    }
+
+}
---
+++ pt_231/pt_231/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static pt_231.Person;
+
+namespace pt_231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample ";
+            employee.LastName = "Student";
+            employee.SayName();
+        }    
+    }
+}
