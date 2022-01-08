7e0cade6845cd33a811449bcad172bbabf87a7fe
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-07 16:28:13 -0800

    Add files via upload

---
+++ pt_224/pt_224/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pt_224
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Choose a number to divide by 2");
+            int UserNumb = Convert.ToInt32(Console.ReadLine());
+            math.MathA(UserNumb);
+
+            Console.WriteLine("Choose a number to multiply by 2");
+            decimal UserNumb2 = Convert.ToDecimal(Console.ReadLine());
+            math.MathA(UserNumb2);
+
+            int initializeD;
+            math.MathB(out initializeD);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ pt_224/pt_224/math.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pt_224
+{
+    class Math
+    {
+        public void MathA(int A)
+        {
+            int result = A / 2;
+            Console.WriteLine(result);
+
+        }
+        public void MathA(decimal A)
+        {
+            int result = Convert.ToInt32(A) * 2;
+            Console.WriteLine(result);
+        }
+
+        public void MathB(out int A)
+        {
+            A = 55;
+            Console.WriteLine(A);
+        }
+    }
+}
