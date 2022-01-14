d063ab4c91f2556ce704ecbe15db5d9908c41f8c
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-13 17:19:30 -0800

    pt_268

---
+++ pt_268/pt_268/Num.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pt_268
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ pt_268/pt_268/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pt_268
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 5;
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
