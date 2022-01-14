8500c8bc2bed57f63677518b518fb7828875bbed
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-13 16:41:15 -0800

    pt262

---
+++ PT_262/PT_262/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PT_262
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the current day of the week");
+            string CurrentDay = Console.ReadLine();
+            try
+            {
+                DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), CurrentDay);          
+                Console.WriteLine(day + " is a valid day");
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sorry, but " + CurrentDay + "is not a valid day, no soup for you");
+            }
+            
+        }
+    }
+}
---
+++ PT_262/PT_262/Week.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PT_262
+{
+    public class Week
+    {
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
