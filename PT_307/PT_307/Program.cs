5b67b00428882cbca29b89d818040033fc9b704f
Brycenbeans <93954185+Brycenbeans@users.noreply.github.com>
2022-01-18 16:43:57 -0800

    pt_307 dateTime

---
+++ PT_307/PT_307/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace PT_307
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime today = DateTime.Now;
+            DateTime whenTime = today.AddHours(userNumber);
+            Console.WriteLine(whenTime);
+        }
+    }
+}
